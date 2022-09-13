0f92daae2c1de2162efc98ba83f9e790a2be707f
nickrubenstein <nick@rubenstein.dev>
2022-09-13 14:37:34 -0500

    Add basic steps  to deploy to a raspberry pi

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o xtrace
+
+readonly TARGET_HOST=pi@192.168.86.50
+readonly TARGET_PATH=/home/pi/PaP-FS
+readonly TARGET_ARCH=armv7-unknown-linux-musleabihf
+readonly SOURCE_PATH=./target/${TARGET_ARCH}/release/plug-and-play-fs
+
+cargo build --release --target=${TARGET_ARCH}
+rsync ${SOURCE_PATH} ${TARGET_HOST}:${TARGET_PATH}
+rsync -r templates ${TARGET_HOST}:${TARGET_PATH}
+# ssh -t ${TARGET_HOST} ${TARGET_PATH}/plug-and-play-fs
