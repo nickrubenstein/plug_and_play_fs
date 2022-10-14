33bf5b42a2df8304d40d5a8b69a2f3e3be220ec4
nickrubenstein <nick@rubenstein.dev>
2022-10-14 18:05:05 -0500

    add env var for path to cross compiler

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,11 @@ set -o nounset
 set -o pipefail
 set -o xtrace
 
-readonly TARGET_HOST=pi@192.168.86.50
+readonly TARGET_HOST=pi@192.168.86.24
 readonly TARGET_PATH=/home/pi
 readonly TARGET_ARCH=armv7-unknown-linux-musleabihf
 readonly SOURCE_PATH=./target/${TARGET_ARCH}/release/plug-and-play-fs
+export CC=/usr/local/bin/arm-linux-musleabihf-gcc-8
 
 cargo build --release --target=${TARGET_ARCH}
 rsync ${SOURCE_PATH} ${TARGET_HOST}:${TARGET_PATH}
